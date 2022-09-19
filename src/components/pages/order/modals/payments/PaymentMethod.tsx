4c45fbe2180c74421f53ec8238dd041de6d8601d
Michael Montero <monterom334@gmail.com>
2022-09-19 18:41:31 -0400

    fix payment option selection

--- src/components/pages/order/modals/payments/PaymentMethod.tsx
+++ src/components/pages/order/modals/payments/PaymentMethod.tsx
@@ -16,7 +16,7 @@ export const PaymentMethods: React.FC<PaymentMethodsProps> = memo(function Payme
 }: PaymentMethodsProps) {
   const isDesktop = useResponsive('up', 'sm');
 
-  const [paymentsMethods, setPaymentMethods] = useState(PaymentsOptions());
+  const [paymentsMethods, setPaymentMethods] = useState(PaymentsOptions);
   const [isSelected, setIsSelected] = useState<boolean>(false);
 
   const { changeStep, step, handleMethodChange } = useContext(PaymentContext);
@@ -40,53 +40,69 @@ export const PaymentMethods: React.FC<PaymentMethodsProps> = memo(function Payme
     [step]
   );
 
+  const PaymentOptionItem = ({ id, icons, title, active, disabled, description, onClick }) => {
+    const clickHandle = useCallback(() => {
+      onClick(id);
+    }, [id]);
+
+    return (
+      <div onClick={clickHandle}>
+        <OptionStyle
+          key={title}
+          sx={{
+            ...(active && {
+              boxShadow: 'rgb(145 158 171 / 16%) 0px 20px 40px -4px'
+            })
+          }}
+        >
+          <FormControlLabel
+            disabled={disabled && !active}
+            checked={active}
+            value={id}
+            control={<Radio checkedIcon={<Iconify icon={'eva:checkmark-circle-2-fill'} />} />}
+            label={
+              <Box sx={{ ml: 1 }}>
+                <Typography variant="subtitle2">{title}</Typography>
+                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                  {description}
+                </Typography>
+              </Box>
+            }
+            sx={{ flexGrow: 1, py: 3 }}
+          />
+
+          {isDesktop && (
+            <Stack direction="row" spacing={1} flexShrink={0}>
+              {icons?.map(icon => (
+                <Image key={icon} sx={{ width: '30px' }} alt={title} src={icon} />
+              ))}
+            </Stack>
+          )}
+        </OptionStyle>
+      </div>
+    );
+  };
+
   return (
     <React.Fragment>
       <RadioGroup row>
         <Stack spacing={2} sx={{ width: '100%' }}>
           {paymentsMethods?.map((method, i) => {
-            const { id, title, icons, description } = method;
+            const { id, active, title, icons, description } = method;
             return (
               <React.Fragment key={id}>
                 {method.active === false && isSelected ? (
                   <React.Fragment></React.Fragment>
                 ) : (
-                  <div onClick={() => handleSelectPayment(id)}>
-                    <OptionStyle
-                      key={title}
-                      sx={{
-                        ...(method.active && {
-                          boxShadow: 'rgb(145 158 171 / 16%) 0px 20px 40px -4px'
-                        })
-                      }}
-                    >
-                      <FormControlLabel
-                        disabled={disabled && !method.active}
-                        checked={method.active}
-                        value={id}
-                        control={
-                          <Radio checkedIcon={<Iconify icon={'eva:checkmark-circle-2-fill'} />} />
-                        }
-                        label={
-                          <Box sx={{ ml: 1 }}>
-                            <Typography variant="subtitle2">{title}</Typography>
-                            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                              {description}
-                            </Typography>
-                          </Box>
-                        }
-                        sx={{ flexGrow: 1, py: 3 }}
-                      />
-
-                      {isDesktop && (
-                        <Stack direction="row" spacing={1} flexShrink={0}>
-                          {icons?.map(icon => (
-                            <Image key={icon} sx={{ width: '30px' }} alt={title} src={icon} />
-                          ))}
-                        </Stack>
-                      )}
-                    </OptionStyle>
-                  </div>
+                  <PaymentOptionItem
+                    id={id}
+                    icons={icons}
+                    active={active}
+                    title={title}
+                    description={description}
+                    disabled={disabled}
+                    onClick={handleSelectPayment}
+                  />
                 )}
               </React.Fragment>
             );
--- src/components/pages/order/modals/payments/PaymentsOptions.tsx
+++ src/components/pages/order/modals/payments/PaymentsOptions.tsx
@@ -1,32 +1,29 @@
 import React from 'react';
 import { OrderPayment } from '../../../../../enums/order.payment';
 
-export const PaymentsOptions = () => {
-  let options = [
-    {
-      id: 1,
-      title: 'Efectivo',
-      methodId: OrderPayment.CASH,
-      icons: ['/illustrations/ic_cash.svg'],
-      description: 'Efectivo en caja',
-      active: false
-    },
-    {
-      id: 5,
-      title: 'Tarjeta de credito al Mensajero',
-      methodId: OrderPayment.TC,
-      icons: ['/illustrations/ic_visa.svg', '/illustrations/ic_mastercard.svg'],
-      description: 'Pagar en Tarjeta de Credito al Mensaje cuando se haga la entrega',
-      active: false
-    },
-    {
-      id: 4,
-      title: 'Efectivo al Mensajero',
-      methodId: OrderPayment.CASH_DELIVERY,
-      icons: [],
-      description: 'Pagar en efectivo cuando al Mensaje cuando se haga la entrega',
-      active: false
-    }
-  ];
-  return options;
-};
+export const PaymentsOptions = [
+  {
+    id: 1,
+    title: 'Efectivo',
+    methodId: OrderPayment.CASH,
+    icons: ['/illustrations/ic_cash.svg'],
+    description: 'Efectivo en caja',
+    active: false
+  },
+  {
+    id: 5,
+    title: 'Tarjeta de credito al Mensajero',
+    methodId: OrderPayment.TC,
+    icons: ['/illustrations/ic_visa.svg', '/illustrations/ic_mastercard.svg'],
+    description: 'Pagar en Tarjeta de Credito al Mensaje cuando se haga la entrega',
+    active: false
+  },
+  {
+    id: 4,
+    title: 'Efectivo al Mensajero',
+    methodId: OrderPayment.CASH_DELIVERY,
+    icons: [],
+    description: 'Pagar en efectivo cuando al Mensaje cuando se haga la entrega',
+    active: false
+  }
+];
