dd1f38638e7258fa4e56223f783b307b2e045281
Michael Montero <monterom334@gmail.com>
2022-10-03 15:46:07 -0400

    remove dev dependency

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,6 @@ module.exports = {
     'plugin:react-perf/recommended',
     'prettier'
   ],
-  parser: '@typescript-eslint/parser',
   plugins: ['react', 'react-hooks'],
   rules: {
     'prettier/prettier': 0,
@@ -20,13 +19,6 @@ module.exports = {
     'react/prop-types': 0,
     'no-shadow': 'off', // turn off eslint/no-shadow
     '@typescript-eslint/no-shadow': ['error'], // turn on typescript-eslint/no-shadow
-    'react/jsx-no-bind': [
-      1,
-      {
-        // no arrow functions / function declared inline in JSX
-        ignoreRefs: true // https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/jsx-no-bind.md
-      }
-    ],
     'react-perf/jsx-no-new-object-as-prop': ['warn'],
     'react-perf/jsx-no-new-array-as-prop': ['warn']
   },
